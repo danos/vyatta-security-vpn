796a848401a33ae126b810380805b315ad34023a
Mike Manning <mmanning@vyatta.att-mail.com>
2020-07-30 15:42:49 +0100

    generate vpn x509 key-pair: do not overwrite files
    Do not overwrite the key and csr files if they already exist.
    
    VRVDR-52389

--- scripts/vyatta-gen-x509-keypair.sh
+++ scripts/vyatta-gen-x509-keypair.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2017-2019, AT&T Intellectual Property.  All rights reserved.
+# Copyright (c) 2017-2020, AT&T Intellectual Property.  All rights reserved.
 # Copyright (c) 2014-2016 by Brocade Communications Systems, Inc.
 # All rights reserved.
 #
@@ -15,6 +15,7 @@ set -e
 CN=$1
 KEYTYPE=$2
 KEYPARAM1=$3
+DIR=/config/auth
 
 genkeypair (){
   case "$KEYTYPE" in
@@ -42,9 +43,14 @@ genkeypair (){
 
   esac
 }
-if [ -f /config/auth/$CN.csr ]; then
-  read -p "A certificate request named $CN.csr already exists. Overwrite (y/n)?"
-  [[ $REPLY != y && $REPLY != Y ]] || genkeypair
+if [ -L $DIR/$CN.csr ] || [ -L $DIR/$CN.key ]; then
+  echo "Symbolic link found for key and/or csr file, remove these and try again:"
+  echo " $DIR/$CN.csr"
+  echo " $DIR/$CN.key"
+elif [ -e $DIR/$CN.csr ] || [ -e $DIR/$CN.key ]; then
+  echo "A key and/or csr file already exist, remove these and try again:"
+  echo " $DIR/$CN.csr"
+  echo " $DIR/$CN.key"
 else 
   genkeypair
 fi
